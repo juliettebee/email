94e09a76979fb02cf9ae79c88c16ece59d4cf98e
Juliette <git@juliette.page>
2020-12-25 16:31:12 -0500

    It works

--- email.v
+++ email.v
@@ -1,8 +1,18 @@
 module main
 
 import smtp_server 
+import os
+import json
+
+struct Settings {
+    email_dir string [json:emailDir]
+}
 
 fn main() {
-    smtp_server.start()
+    // Loading settings
+    filename := './settings.json'
+    settings_file := os.read_file(filename) or { panic('Unable to find settings file! Please create settings.json') }
+    settings_json := json.decode(Settings, settings_file) or {panic('Unable to parse settings')}
+    smtp_server.start(settings_json.email_dir)
 }
 
--- smtp.v
+++ smtp.v
@@ -1,6 +1,9 @@
 module smtp_server
 
 import net
+import time
+import os
+import json
 
 struct Email {
     pub mut:
@@ -8,18 +11,24 @@ struct Email {
         to []string
         data string
 }
-pub fn start() {
+
+struct Email_file {
+    pub mut:
+        files []string    
+}
+
+pub fn start(email_dir string) {
     // Opens port 25
     l := net.listen_tcp(25) or { panic(err) }
     // Wait for request
     for {
         // Accept request
         new_request := l.accept() or { continue } 
-        handle(new_request)
+        handle(new_request, email_dir)
     }
 }
 
-fn handle(connection net.TcpConn) {
+fn handle(connection net.TcpConn, email_dir string) {
     // Sending hello
     connection.write_str('220 smtp.juliette.page ESMTP Postfix\n')
     // Creating a blank email
@@ -81,14 +90,17 @@ fn handle(connection net.TcpConn) {
             }
             email.to << to
             connection.write_str('250 OK\n')
+            // Enabling data modde if user requests
         } else if command[0..4] == ['D','A','T','A'] {
            data_mode = true 
            connection.write_str('354 Send message content; end with .!\n') 
         } else if data_mode {
+            // Getting data
             mut data := ''
             for letter in command {
                     data += letter.str()
             }
+            // Seeing if they endded it
             if data[0].str() == '.' {
                 if data[1].str() == '!' { 
                     print('Out of data')
@@ -98,11 +110,25 @@ fn handle(connection net.TcpConn) {
             } else {
                 email.data += data
             }
+            // Quit command
         } else if command[0..4] == ['Q','U','I','T'] {
             connection.write_str('221 Bye\n')
             connection.close()
             break
         }
     }
-    println(email)
+    // Saving email
+    time_now := time.now().format_ss_milli()
+    email_file_name := '$email_dir/${time_now}.json'
+    mut email_file := os.create(email_file_name) or {panic('Incorrect settings! Unable to create file') }
+    email_file.write_str(json.encode(email)) 
+    email_file.close()
+    // Adding to the list
+    email_list_file_name := '$email_dir/emails.json'
+    mut email_list := os.open_file(email_list_file_name, 'w+') or {panic('Incorrect settings! Unable to Open emails.json')}
+    mut email_list_contents := os.read_file(email_list_file_name) or { panic('Bad settings!')}
+    mut email_list_json := json.decode(Email_file, email_list_contents) or { panic('unable to parse settings')} 
+    email_list_json.files << '${time_now}.json' 
+    email_list.write_str(json.encode(email_list_json))
+    email_list.close()
 }
