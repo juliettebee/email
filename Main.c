67d798ee53fa024e44533146c90de9407c0f5884
Juliette <git@juliette.page>
2021-02-25 17:33:44 -0500

    Now a deamon

--- Main.c
+++ Main.c
@@ -1,6 +1,32 @@
 #include "Main.h"
 
-int main() {
+int main( int argc, char *argv[] ) {
+    if (argc != 2) {
+        printf("Please include the folder where you want emails to go!");
+        exit(-1);
+    }
+    // Creating a deamon
+    pid_t process_id = 0;
+    pid_t sid = 0;
+    // Create child process
+    process_id = fork();
+    // Checking for error
+    if (process_id < 0) {
+        printf("Error in forking!");
+        exit(-1);
+    }
+    if (process_id > 0) {
+        printf("Bye!");
+        exit(0);
+    }
+    umask(0);
+    sid = setsid();
+    if(sid < 0)
+        exit(-1);
+    chdir(argv[1]);
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
     smtpServer();
     return 0;
 }
