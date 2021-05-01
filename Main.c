af9f43cae601dcb4ac6e331c74e0847aa13cbfe3
Juliette <git@juliette.page>
2021-05-01 12:31:47 -0400

    Few updates

--- Main.c
+++ Main.c
@@ -2,19 +2,42 @@
 
 int main (int argc, char *argv[]) {
     // Getting config
-    if (argc < 2) {
-        printf("✉️  Juliette's SMTP server\n./email [Domain] [Discord style webhook url (Optional)]\n");
+    if (argc < 3) {
+        printf("✉️  Juliette's SMTP server\n./email [Email storage path] [Domain]\n");
         return 0;
     }
 
-    if (strlen(argv[1]) > 64) {
+    if (strlen(argv[2]) > 64) {
         printf("The domain cannot be above 64 characters!\n");
         return 0;
     }
 
     ServerConfig config = {0};
-    strcpy(config.domain, argv[1]);
-    
+    strcpy(config.domain, argv[2]);
+
+    printf("%s", argv[1]);
+    // Creating a deamon
+    pid_t process_id = 0;
+    pid_t sid = 0;
+    process_id = fork();
+    if (process_id < 0) {
+        printf("Error in forking!");
+        exit(-1);
+    }
+    if (process_id > 0) {
+        printf("Bye!");
+        exit(0);
+    }
+    umask(0);
+    sid = setsid();
+    if (sid < 0)
+        exit(-1);
+
+    chdir(argv[1]);
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+        
     // todo: daemon this
     smtpServer(config);
 }
--- Makefile
+++ Makefile
@@ -5,3 +5,10 @@ all: compile
 
 compile: Main.c smtpServer/smtpServer.c
 	$(cc) $(cflags) $?
+
+deploy: compile
+	iptables -P INPUT ACCEPT
+	iptables -P OUTPUT ACCEPT
+	iptables -P FORWARD ACCEPT
+	iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 25 -j REDIRECT --to-port 2525
+	iptables-save
