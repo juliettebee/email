bb1b5a7547182592ae794162b1d3b058af7df2de
Juliette <git@juliette.page>
2021-06-17 10:33:22 -0400

    Update

--- popServer/Sources/popServer/JSocket.swift
+++ popServer/Sources/popServer/JSocket.swift
@@ -47,3 +47,19 @@ struct JSocket {
         #endif
     }
 }
+
+extension String {
+    func write (_ fd: JSocket) {
+        #if os(Linux)
+        let out = Glibc.write(fd.fd, self, self.lengthOfBytes(using: .utf8))
+        #elseif os(macOS)
+        let out = Darwin.write(fd.fd, self, self.lengthOfBytes(using: .utf8))
+        #endif
+        
+        print(out)
+        
+        if out == -1 {
+            print("Error \(errno)")
+        }
+    }
+}
