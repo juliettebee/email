f1eb79b9e6783fdea9e1a01e9431d5300c971a72
Juliette <git@juliette.page>
2021-06-18 08:51:52 -0400

    Helo and quit

--- Sources/Email/handleConnection.swift
+++ Sources/Email/handleConnection.swift
@@ -9,4 +9,24 @@ import Foundation
 
 func handleConnection (connection: JSocket, folder: URL) {
     "220 ESMTP Juliette's SMTP Server \n".write(connection)
+    
+    while (true) {
+        let input = connection.read()
+        var command: String = ""
+        if input.contains(":") {
+            command = input.components(separatedBy: ":")[0]
+        } else {
+            command = String(input.prefix(4))
+        }
+        
+        switch command {
+        case "HELO":
+            "250 Hello\n".write(connection)
+        case "QUIT":
+            connection.close()
+            return
+        default:
+            "500 I don't know that\n".write(connection)
+        }
+    }
 }
--- tests/tests.exs
+++ tests/tests.exs
@@ -6,6 +6,29 @@ defmodule ConnectionTests do
     test "Server is up" do
       {:ok, socket} = :gen_tcp.connect('localhost', 2525, [:binary, active: false])
       {:ok, message} = :gen_tcp.recv(socket, 0)
+      :gen_tcp.send(socket, "QUIT")
       assert message == "220 ESMTP Juliette's SMTP Server \n"
     end
+end
+
+defmodule CommandTests do
+  use ExUnit.Case, async: true
+
+  test "Invalid command" do
+    {:ok, socket} = :gen_tcp.connect('localhost', 2525, [:binary, active: false])
+    {:ok, _} = :gen_tcp.recv(socket, 0)
+    :gen_tcp.send(socket, "BAD COMMAND: BAD")
+    {:ok, message} = :gen_tcp.recv(socket, 0)   
+    :gen_tcp.send(socket, "QUIT") 
+    assert message == "500 I don't know that\n"
+  end
+
+  test "HELO" do
+    {:ok, socket} = :gen_tcp.connect('localhost', 2525, [:binary, active: false])
+    {:ok, _} = :gen_tcp.recv(socket, 0)
+    :gen_tcp.send(socket, "HELO")
+    {:ok, message} = :gen_tcp.recv(socket, 0)
+    :gen_tcp.send(socket, "QUIT")
+    assert message == "250 Hello\n"
+  end
 end
